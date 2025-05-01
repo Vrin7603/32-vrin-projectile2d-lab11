55d84afa29c427322653f56a2926c6a93a6b7a3b
Vrin7603 <vrin.tipt@bumail.net>
2025-05-01 19:54:09 +0700

    CanShootandAddName

--- Assets/Scripts/Projectile2D.cs
+++ Assets/Scripts/Projectile2D.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Projectile2D : MonoBehaviour
@@ -6,11 +7,6 @@ public class Projectile2D : MonoBehaviour
     [SerializeField] GameObject target;
     [SerializeField] Rigidbody2D bulletPrefab;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -26,7 +22,24 @@ public class Projectile2D : MonoBehaviour
             {
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
                 Debug.Log("hit" + hit.collider.name);
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+                shootBullet.linearVelocity = projectileVelocity;
             }
         }
     }
-}
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+
+        return projectileVelocity;
+    }
+} 
