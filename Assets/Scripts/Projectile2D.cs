dc54c62542b28fcd81e49ddf4c56aef73dc78dbc
Vrin7603 <vrin.tipt@bumail.net>
2025-05-01 19:16:19 +0700

    addProjetile2DScripttoShooter

---
+++ Assets/Scripts/Projectile2D.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Projectile2D : MonoBehaviour
+{
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
