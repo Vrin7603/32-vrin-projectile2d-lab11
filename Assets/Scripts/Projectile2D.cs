de2316df9600029f05d03bb94aca23b35c069a82
Vrin7603 <vrin.tipt@bumail.net>
2025-05-01 19:24:49 +0700

    updateScriptMouseDetect

--- Assets/Scripts/Projectile2D.cs
+++ Assets/Scripts/Projectile2D.cs
@@ -15,6 +15,18 @@ public class Projectile2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if(hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit" + hit.collider.name);
+            }
+        }
     }
 }
